e21f08a8f76e2e379e5a5a1ebfca573c40966757
Layout example <ger_boyle@yahoo.ie>
2018-09-23 19:59:13 +0000

    Modified for invalid user search

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -1,13 +1,17 @@
 class HeadlinesController < ApplicationController
     
    def search
-        if params[:headline].present?
-          @headline = Headline.new_from_lookup(params[:headline]).as_json
-          #render json: @headline
-          render 'users/my_news'
-        else
-          flash[:danger] = "You have entered an empty search string"
-          redirect_to my_news_path
-        end
+      if params[:headline].present?
+        @headline = Headline.new_from_lookup(params[:headline]).as_json
+          if @headline
+            render 'users/my_news'
+          else
+            flash[:danger] = "You have entered an incorrect News ticker"
+            redirect_to my_news_path
+          end
+      else
+        flash[:danger] = "You have entered an empty search string"
+        redirect_to my_news_path
+      end
    end
 end
--- app/models/headline.rb
+++ app/models/headline.rb
@@ -1,7 +1,11 @@
 class Headline < ApplicationRecord
     
         def self.new_from_lookup(news_ticker)
-            n = News.new(ENV['API_KEY'])
-            n.get_top_headlines(sources: news_ticker)
+            begin
+                n = News.new(ENV['API_KEY'])
+                n.get_top_headlines(sources: news_ticker)
+            rescue Exception => e
+                return nil
+            end
         end
 end
